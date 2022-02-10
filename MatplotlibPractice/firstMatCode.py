8001250cde4dd23e39ab51ea68ceb51bd68fdbf9
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-02-10 11:44:14 +0600

    Matplotlib coding start

---
+++ MatplotlibPractice/firstMatCode.py
@@ -0,0 +1,20 @@
+# line chart
+
+from matplotlib import pyplot as plt
+x=[5,2,9,4,1,6]
+y=[10,8,4,2,9,7]
+#plt.plot(x,y)
+#plt.show()
+
+# bar chart
+#plt.bar(x,y)
+#plt.show()
+
+
+# Histogram chart
+# plt.hist(y)
+# plt.show()
+
+# Scarter Chart
+plt.scatter(x,y)
+plt.show()
