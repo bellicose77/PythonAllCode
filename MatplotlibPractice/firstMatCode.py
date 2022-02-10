c2c8df104b1b5ec248a7ee45c20752ff9b069e31
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-02-10 17:32:14 +0600

    last comment on the day

--- MatplotlibPractice/firstMatCode.py
+++ MatplotlibPractice/firstMatCode.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot
 from matplotlib import pyplot as plt
 x=[5,2,9,4,1,6]
 y=[10,8,4,2,9,7]
+print(type(x))
 #plt.plot(x,y)
 #plt.show()
 
@@ -25,6 +26,6 @@ plt.xlabel("Number of Sales")
 plt.ylabel("Time(Hour)")
 # plt.scatter(x,y)
 # plt.show()
-plt.plot(x,y)
-#plt.plot(x,y,'-.')
+#plt.plot(x,y)
+plt.plot(x,y,'-.')
 plt.show()
