ffae27d09ca5bdd1817cdb252129c14a5d271c1a
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-02-12 11:24:21 +0600

    figureSize

---
+++ CustomizingPlots/PlotSize.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+x = [1, 2, 3, 4, 5]
+y = [1, 2, 3, 4, 5]
+plt.figure().set_figwidth(4)
+plt.figure().set_figheight(8)     # changing default size of the figure
+plt.plot(x,y)
+plt.show()
